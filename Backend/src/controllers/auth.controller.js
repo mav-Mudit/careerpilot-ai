7f50793411fc51606f581d3e8890ad9a5cc3179b
Mudit Tandon <107250888+mav-Mudit@users.noreply.github.com>
2026-05-19 14:42:17 +0200

    Enhance cookie security for token in auth controller
    Add cookie options for security in authentication responses.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -46,7 +46,11 @@ async function registerUserController(req, res) {
       expiresIn: "1d",
     },
   );
-  res.cookie("token", token);
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
   res.status(201).json({
     message: "User registered successfully",
@@ -88,7 +92,11 @@ async function loginUserController(req, res) {
       expiresIn: "1d",
     },
   );
-  res.cookie("token", token);
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(200).json({
     message: "User logged in successfully",
     user: {
